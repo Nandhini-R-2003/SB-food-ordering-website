d64f2919c486c3ccbe69145c9a3293dbecbb6c8d
Nandhini-R-2003 <nandhiniraja2710@gmail.com>
2024-11-18 11:14:23 +0530

    first commit

---
+++ SB Food Delivery/README.md
@@ -0,0 +1,2 @@
+# Food-Ordering-App-MERN
+Demo - <a href="https://drive.google.com/file/d/14yNJVY_hlLX_PLWSEjAxPOBiWHe0-VLz/view?usp=sharing">view video</a>
---
+++ SB Food Delivery/client/build/precache-manifest.47219c9e090f2ce53f1f84404633e224.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ac00a4abe12960e8e31a82328b3520",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40c89493618a9b53b415",
+    "url": "/static/css/main.23250ac1.chunk.css"
+  },
+  {
+    "revision": "0ed644ef2d2da43df74f",
+    "url": "/static/js/2.78b98c6f.chunk.js"
+  },
+  {
+    "revision": "577ebce57e241eef3670",
+    "url": "/static/js/3.75735a85.chunk.js"
+  },
+  {
+    "revision": "40c89493618a9b53b415",
+    "url": "/static/js/main.ccfb5365.chunk.js"
+  },
+  {
+    "revision": "4c1173f249bd58e4fc3d",
+    "url": "/static/js/runtime~main.ce801184.js"
+  }
+]);
